a146a109d6c649fdb1dc001d7149190b60286630
NAVEENKUMAR2003301 <livnaveen@gmail.com>
2026-01-19 10:27:46 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,72 @@
+// // // variable
+
+// // // var   - function scope , global scope
+
+// // // let   - block scope
+
+// // // const - block scope
+
+
+// // // rules
+
+// // // var
+
+// // var chocolateBox  = 10         // declration // intialiazation
+
+// // chocolateBox      = 15         // reuse   // reIntialazation
+
+// // var chocolateBox  = 20         // reDeclration
+
+
+// // console.log(chocolateBox)
+
+
+// // // let 
+
+// // let a = 12 // dec   // intia
+
+// // a     = 15 // reuse // reIntia
+
+// // // let a = 50 not reDecl
+
+// // console.log(a);
+
+
+// // // const 
+
+// // const b = 21 // decle  // intia
+
+// // // b       = 23  not accept reuse , reIntialiazation
+
+// // // const b = 30  // not accept reDec
+
+// // console.log(b)
+
+
+
+// printing statement
+
+// 1. console.log()
+
+// let a = 10
+
+// console.log(a);
+// console.log(100)
+
+
+// 2. alert()
+
+// alert("hi hello there")
+
+// 3. confirm()
+
+// confirm("did you like js")
+
+
+// 4. prompt()
+
+// prompt("what is your name")
+
+// 5. document.writeln()
+
+// document.writeln("hello")
