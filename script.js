e661e9e51b2be2548b1b929a224e7bbe11c6bf7c
NAVEENKUMAR2003301 <livnaveen@gmail.com>
2026-01-22 11:57:02 +0530

    function scopes , hoisting

--- script.js
+++ script.js
@@ -420,6 +420,420 @@ console.log(typeof(true + [1,2]));
 console.log(typeof(true + {w:2}));
 
 
+console.clear();
+
+
+
+// Explicit Type Conversion
+
+// number
+
+// (Number) constractor
+
+console.log(1 + Number("1"));
+console.log(Number());
+console.log(Number(""));
+console.log(Number("123"));
+console.log(Number("abc"));
+console.log(Number(12));
+console.log(Number(true));
+console.log(Number(false));
+console.log(Number(undefined));
+console.log(Number(null));
+console.log(Number([19989,56]));
+console.log(Number({k:1}));
+
+
+// boolean 
+
+// (Boolean)
+
+console.log(Boolean(""));
+console.log(Boolean());
+console.log(Boolean("1"));
+console.log(Boolean("abc"));
+console.log(Boolean(12));
+console.log(Boolean(-1));
+console.log(Boolean(0));
+console.log(Boolean(true));
+console.log(Boolean(false));
+console.log(Boolean(undefined));
+console.log(Boolean(null));
+console.log(Boolean([1,2]));
+console.log(Boolean({k:2}));
+
+
+console.clear();
+
+
+
+// flow control statement 
+
+// 1. conditional statement 
+
+// 1. if statement
+
+//  if(condtion){// statement} , condition true = allow 
+
+// example
+
+if(5=="5"){
+
+    console.log("true only allow");
+    
+}
+
+// 2. if else statement
+
+//  if(condtion){// statement}else{// statement} , condition true = allow if inside , false = allow else inside
+
+// example
+
+if(0){
+    console.log("true");
+    
+}else{
+    console.log("false");
+    
+}
+
+// 3. else if statement
+
+let hour = 23
+
+if(hour >= 1 && hour <= 6){
+    console.log("good morning");
+    
+}else if(hour >= 7 && hour <= 12){
+    console.log("morning");
+    
+}else if(hour >= 13 && hour <= 16){
+    console.log("good Afternoon");
+    
+}else if(hour >= 17 && hour <= 22){
+    console.log("good evening");
+    
+}else{
+    console.log("good night");
+    
+}
+
+// 4. nested if statement
+
+// let age  =prompt("enter your Age :- ")
+// let height = prompt("enter your height :- ")
+// let weight =prompt("enter your weight :-  ")
+
+// if(age >= 18){
+//     if(height >= 155){
+//         if(weight >= 55){
+//             alert("your eligible");
+            
+//         }else{
+//             alert("your weight is not eligible");
+            
+//         }
+//     }else{
+//         alert("your height is not eligible");
+        
+//     }
+// }else{
+//     alert("your age is not eligible ");
+    
+// }
+
+// 5. switch statement
+
+// switch(condition){
+//     case value : "statement" ; break;
+//     case value : "statement" ; break ; 
+// }
+
+
+// let trafficLight = "yellow"
+
+// switch(trafficLight){
+//     case "red" : console.log("vechile stop"); break ;
+//     case "yellow" : console.log("vechile start"); break ;
+//     case "green" : console.log("vechile go"); break;
+
+//     default : console.log("go");
+    
+    
+    
+    
+// }
+
+
+
+// 2. looping statement 
+
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+console.log(5);
+
+
+
+// for loop
+
+// for(intialiazation ; condition ; iteration){// statement}
+
+for(let a = 1 ; a <= 5 ; a++){
+    console.log(a); // 1 2 3 4 5
+    
+}
+
+// let a = 1 ; 1 <= 5 = true ; 1++ = 2
+// let a = 2 ; 2 <= 5 = true ; 2++ = 3
+// let a = 3 ; 3 <= 5 = true ; 3++ = 4
+// let a = 4 ; 4 <= 5 = true ; 4++ = 5
+// let a = 5 ; 5 <= 5 = true ; 5++ = 6
+// let a = 6 ; 6 <= 5 = false.
+
+console.clear();
+
+
+// while loop 
+
+// intialiazation
+
+// while(condition){ // statement   // iteration }
+
+// example
+
+var a2 = 5
+
+while(a2 >= 20){
+
+    console.log(a2);
+
+    a2--
+    
+}
+
+// a2 = 5 ; 5 >= 0 = true  ; 5 ; 5-- = 4
+// a2 = 4 ; 4 >= 0 = true  ; 4 ; 4-- = 3
+// a2 = 3 ; 3 >= 0 = true  ; 3 ; 3-- = 2
+// a2 = 2 ; 2 >= 0 = true  ; 2 ; 2-- = 1
+// a2 = 1 ; 1 >= 0 = true  ; 1 ; 1-- = 0
+// a2 = 0 ; 0 >= 0 = true  ; 0 ; 0-- = -1
+// a2 = -1 ; -1 >= 0 = false . 
+
+
+// do while loop 
+
+// syntex
+
+// intialiazation 
+
+// do{
+//     // statement
+
+//     iteration
+// }
+
+// while(condition)
+
+let a3 = 5 
+
+do {
+  console.log(a3);
+
+  a3--;
+} 
+while (a3 >= 10);
+
+// for of loop
+
+// array , string , function
+
+let str = "javascript"
+
+// syntex 
+
+for(let a of str){
+
+    console.log(a);
+    
+}
+
+let arr1 = ["apple","orange","banana"]
+
+for(let b of arr1){
+
+    console.log(b);
+    
+}
+
+// for in loop - object
+
+let obj = {
+    name1 : "john",
+    salery : 100000,
+    role   : "developer"
+} 
+
+for(let c in obj){
+
+    console.log(obj[c]);
+    
+}
+
+console.clear();
+
+
+
+// function 
+
+// syntex 
+
+
+// function one(parameter){
+//     // statement
+// }
+
+
+function one(){
+    console.log("hello");
+    
+}
+
+one()
+
+
+function two(parameter){
+    console.log(parameter);
+    
+}
+
+two("argument")
+
+
+function student(name1,group){
+    console.log("name :-",name1);
+    console.log("group :-",group);
+    
+}
+
+student("kamal","ece")
+student("praveen","cse")
+
+
+let emp = {
+    name1 : "danish",
+    role  : "developer"
+}
+
+function outer(){
+
+    console.log(emp.name1);
+    
+}
+
+outer()
+
+console.clear();
+
+
+
+// types of function 
+
+// named function
+named("argument")
+
+function named(parameter){
+
+    console.log(
+        "named function"
+    );
+    
+}
+
+
+// annonymous function 
+
+var annonymous = function (parameter){
+    
+    console.log("annonymous function");
+    
+}
+
+annonymous("argument")
+
+
+
+
+
+// arrow function
+
+var arrow = (parameter)=>{
+    
+    console.log("arrow function");
+    
+}
+
+arrow("argument")
+
+
+// // scopes 
+
+// // var    - global scope 
+
+// // let    - block scope
+
+// // const  - block scope
+
+
+// function scopeCheck(){
+//     if(true){
+//         var a11 = "global scope"
+//         let b111 = "block scope"
+//         const c6 = "block scope"
+//         console.log(b111);
+//         console.log(c6);
+//     }
+
+    
+
+
+    
+//     console.log(a11);
+    
+    
+// }
+
+// scopeCheck()
+
+// // hoisting
+
+// console.log(ab);
+
+// var ab = "global scope11"
+
+
+
+
+// let ac = "block scope11"
+// console.log(ac);
+
+
+
+
+// const ad = "block scope"
+// console.log(ad);
+
+
+
+
+
+
+
+
+
+
+
 
 
 
